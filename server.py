6096d58588a94db36ac84e9b038f7b7aae673b4a
DavidCuentasMar <david.cuentas@agswebconsulting.com>
2019-10-02 22:43:44 -0500

    Templates & Init Form

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import render_template
+
+import forms
 
 app = Flask(__name__)
+#app = Flask(__name__, template_folder="folder name") IN CASE TEMPLASTES FOLDER IS NOT NAME TEMPLATES
 @app.route('/')
 def index():
-    return 'Flask'
-app.run()
+    fileform = forms.FileForm()
+    title = 'Flask'
+    return render_template('index.html',title = title, form = fileform)
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
